2952923606675f75da14279c10eae12e18a475e5
Fredyspt <fredyfps@gmail.com>
2021-09-03 11:35:55 -0600

    [ADD] Error middleware layer
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const app = express();
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
 
+const app = express();
 // body parser
 app.use(express.json());
 
 moviesApi(app);
+
+// Error middlewares must go after the routes.
+app.use(logErrors);
+app.use(errorHandler);
+
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
